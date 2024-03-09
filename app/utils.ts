46006575345dca56a9b07357e761bf3dda11dff5
Alexey Selivanov <lazypacman@gmail.com>
2024-03-10 02:01:17 +0400

    Update

--- app/utils.ts
+++ app/utils.ts
@@ -3,9 +3,3 @@ declare module _ {
     table: (size: number, cb: (x: number, y: number) => any) => any[][];
   }
 }
-
-_.mixin({
-  table: (size, cb) => {
-    return _.times(size, (y) => _.times(size, (x) => cb(x, y)));
-  },
-});
